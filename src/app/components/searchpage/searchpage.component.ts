b63eb9a7891c1401a85ac6add549899401eb8934
Chitrank Agnihotri <chitrank2050@gmail.com>
2017-06-25 18:02:42 +0530

    Search user functionality added

--- src/app/components/searchpage/searchpage.component.ts
+++ src/app/components/searchpage/searchpage.component.ts
@@ -13,17 +13,25 @@ import {GithubService} from '../../services/github.service';
 export class SearchpageComponent implements OnInit {
   private _user=[];
   private _repos=[];
+  private _status:boolean;
+  username:string;
 
   constructor(private _githhubserice:GithubService) {
+    this._status=false;
+   }
+
+  _searchUser(){
+    this._status=true;
+    this._githhubserice._updateUser(this.username);
+
     this._githhubserice._getUser().subscribe(user=>{
       this._user=user;
     });
+    
     this._githhubserice._getRepos().subscribe(repos=>{
       this._repos=repos;
-      console.log(repos);
     });
-   }
-
+  }
   ngOnInit() {
   }
 }
--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -6,6 +6,8 @@ import 'rxjs/add/operator/map';
 
 export class GithubService {
   private _username:string;
+  private _client_id = '59fd956e5fd01baa11b8';
+  private _client_secret = '0ed2c7f8c85a3bd96fc6556a42ffb763e663a936';
 
   constructor(private _http:Http) { 
     console.log('Serive Ready');
@@ -13,14 +15,14 @@ export class GithubService {
   }
 
   _getUser(){
-    return this._http.get('http://api.github.com/users/'+this._username)
+    return this._http.get('http://api.github.com/users/'+this._username+'?client_id='+this._client_id+'&client_secret='+this._client_secret)
           .map(res => res.json())
   }
   _getRepos(){
-    return this._http.get('http://api.github.com/users/'+this._username+'/repos')
+    return this._http.get('http://api.github.com/users/'+this._username+'/repos?client_id='+this._client_id+'&client_secret='+this._client_secret)
           .map(res => res.json())
   }
-  updateUser(user:string){
+  _updateUser(user:string){
     this._username=user;
   }
 }
